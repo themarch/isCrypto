fd79c71d4bcabcc40bed37d7b08bca0bae89bb1d
themarch <theomarchandarvierlyon@gmail.com>
2021-02-07 12:28:15 +0100

    update source modules

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 'use strict'
 
-const User = require('./Modules/User')
-const RedisWrite = require ('./Modules/RedisWrite')
-const Tweet = require ('./Modules/Tweet')
-const IsCrypto = require('./Modules/IsCrypto')
+const User = require('./Modules/Twitter/User')
+const RedisWrite = require ('./Modules/Twitter/RedisWrite')
+const Tweet = require ('./Modules/Twitter/Tweet')
+const IsCrypto = require('./Modules/Twitter/IsCrypto')
 
 async function checkAndUpdate() {
     const redisWrite = new RedisWrite()
     const user = new User()
     const nbTweet = await user.getNumberOfTweet('44196397')
-    if (!(await redisWrite.compareValue('NbTweet', nbTweet))) {
+    if (await redisWrite.compareValue('NbTweet', nbTweet)) {
+        redisWrite.redisClose()
         return false
     }
     redisWrite.insertRedis('NbTweet', nbTweet)
+    redisWrite.redisClose()
     return true
 }
 
@@ -22,20 +24,18 @@ async function tweetIsCrypto() {
     const lastTweet = await tweet.getLastTweet('44196397')
     try {
         const containWord = isCrypto.containCrypto(lastTweet)
-        const isBitcoin = isCrypto.isBitcoin(containWord) 
-        console.log(isBitcoin)
+        const whichCrypto = isCrypto.whichCrypto(containWord) 
+        console.log(whichCrypto)
     } catch (e) {
         console.log(e)
     }
 }
 
 async function main() {
-
     const newTweet = await checkAndUpdate()
     if (!newTweet)
         return console.log('No new tweet')
-    await tweetIsCrypto
-    
+    tweetIsCrypto()    
 }
 
 main()
